5aa6ba5ed80e660cd131722231179af80738f46f
esteranodin <esteranodin@hotmail.fr>
2025-02-26 12:25:32 +0100

    ADD: API && endpoint Book

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -2,22 +2,40 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
 use App\Repository\BookRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
+#[ApiResource(
+    operations: [
+        new Get(normalizationContext: ['groups' => ['book:read']]),
+        new GetCollection(normalizationContext: ['groups' => ['book:read']]),
+        new Post(denormalizationContext: ['groups' => ['book:write']]),
+        new Patch(denormalizationContext: ['groups' => ['book:write']]),
+        new Delete(),
+    ]
+)]
 class Book
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['book:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $isbn = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['book:read', 'book:write'])]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::DATE_IMMUTABLE, nullable: true)]
